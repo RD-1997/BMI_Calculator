6b95cc22943446f2878c9f93c6ab774f858c2aa6
RD-1997 <raoul.shaher@gmail.com>
2021-05-02 19:02:58 +0200

    Added a new user input validation function

--- body_mass_index_calculator.py
+++ body_mass_index_calculator.py
@@ -3,43 +3,22 @@ print("#########################################################\n"
       "#########################################################\n")
 
 
+# function to check user input
+def check_user_input(text, unit):
+    while True:
+        value = input("Please enter your {} in {}: ".format(text, unit))
+        try:
+            return float(value)
+        except ValueError:
+            print("Invalid input. Please check your input again.\n")
+
 # asks user to input his height
-height = input("Please enter your height in m: ")
+height = check_user_input("height", "m")
 
-# mechanism to check if the user input is valid input
-while True:
-
-    # checks if the user input is a float
-    # if it is a float it breaks the while loop and proceeds to the next block of code
-    try:
-        float(height)
-        new_height = float(height)
-        break
-    # if the user input is not a float or a number, it prompts the user again for valid input
-    except ValueError:
-        print("Enter a valid height...")
-        height = input("enter your height in m: ")
-        print("")
-        continue
-
-weight = input("Please enter your weight in kg: ")
-
-while True:
-    # checks if the user input is a float
-    # if it is a float it breaks the while loop and proceeds to the next block of code
-    try:
-        float(weight)
-        new_weight = float(weight)
-        break
-    # if the user input is not a float or a number, it prompts the user again for valid input
-    except ValueError:
-        print("Enter a valid weight...")
-        weight = input("enter your weight in kg:")
-        print("")
-        continue
+weight = check_user_input("weight", "kg")
 
 # calculating the body mass index
-body_mass_index = new_weight / new_height**2
+body_mass_index = weight / height**2
 
 # printing out the result to the user
 print("\nYour BMI is " + str(round(body_mass_index, 2)))
