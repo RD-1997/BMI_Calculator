1a25154c8d1ba4e033a096d92c6f40d6b34b9881
RD-1997 <raoul.shaher@gmail.com>
2021-05-02 18:51:48 +0200

    Added a more efficient way of input validation

--- split_payment_calculator.py
+++ split_payment_calculator.py
@@ -2,65 +2,32 @@ print("####################################################\n"
       "######## Welcome to the payment calculator #########\n"
       "####################################################\n")
 
-# asks the user for the total bill
-bill = input("What was the total bill? ")
-
-# mechanism to check if the user input is valid input
-while True:
-    try:
-        # checks if the user input is a float
-        # if it is a float it breaks the while loop and proceeds to the next block of code
-        float(bill)
-        float_bill = float(bill)
-        break
-    except ValueError:
-        # if the user input is not a float or a number, it prompts the user again for valid input
-        print("\nEnter a valid number...")
-        bill = input("What was the total bill? ")
+# function to check if the user input is valid
+def validate_user_input(var, units):
+    while True:
+        value = input('{} in {}: '.format(var, units))
         print("")
-        continue
+        try:
+            return float(value)
+        except ValueError:
+            print("Invalid entry! Please check your input and try again.\n")
+
+
+# asks the user for the total bill
+bill = validate_user_input("What was the total bill", "your currency?")
 
 # asks the user for the tip
-tip = input("How much tip would you like to give? ")
-
-# mechanism to check if the user input is valid input
-while True:
-    try:
-        # checks if the user input is a float
-        # if it is a float it breaks the while loop and proceeds to the next block of code
-        float(tip)
-        float_tip = float(tip)
-        break
-        # if the user input is not a float or a number, it prompts the user again for valid input
-    except ValueError:
-        print("\nEnter a valid number...")
-        tip = input("How much tip would you like to give? ")
-        print("")
-        continue
+tip = validate_user_input("What tip would you like to give", "your currency?")
 
 # asks the user for the amount of people
-total_people = input("How many people to split the bill? ")
+total_people = validate_user_input("How many people to split the bill", "numbers?")
 
-# mechanism to check if the user input is valid input
-while True:
-    try:
-        # checks if the user input is an integer
-        # if it is an integer it breaks the while loop and proceeds to the next block of code
-        int(total_people)
-        int_total_people = int(total_people)
-        break
-        # if the user input is not a float or a number, it prompts the user again for valid input
-    except ValueError:
-        print("\nEnter a valid number...")
-        total_people = input("How many people to split the bill?? ")
-        print("")
-        continue
 
 # calculates the total bill
-total_bill = float_bill + float_tip
+total_bill = bill + tip
 
 # calculates how much everyone should pay
-split_bill = total_bill / int_total_people
+split_bill = total_bill / total_people
 
 # displays the result of how much each person should pay
-print("\nEach person should pay " + str(round(split_bill, 2)))
+print("Each person should pay " + str(round(split_bill, 2)))
